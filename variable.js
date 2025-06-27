0d305ce0874ffab5e166d404bbeadcb36e96c8c7
UmaaMageshwari <umaa0210ammu@gmail.com>
2025-06-27 14:14:25 +0530

    Variable Javascript

---
+++ variable.js
@@ -0,0 +1,23 @@
+const uid="123";
+var firstName="Umaa";
+var lastName="Mageshwari";
+var email="umaa@gmail.com";
+var password="1234";
+var country="India";
+var state="Tamil Nadu";
+var CourseCount=1;
+var isLoggedInFromGoogle=true;
+var isLoggedInFromFB=false;
+
+console.log(`
+    Unique Id:${uid}
+    FirstName:${firstName}
+    LastName:${lastName}
+    email:${email}
+    Password:${password}
+    Country:${country}
+    State:${state}
+    CourseCount:${CourseCount}
+    IsLoggedInFromGoogle:${isLoggedInFromGoogle}
+    IsLoggedInFromFB:${isLoggedInFromFB}
+    `)
