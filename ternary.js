6707d7c0c9e3f9bd2a60e84e723e50da4991d8ff
UmaaMageshwari <umaa0210ammu@gmail.com>
2025-06-30 15:44:16 +0530

    ternary

---
+++ ternary.js
@@ -0,0 +1,13 @@
+var authenicate=false;
+if(authenicate)
+{
+    console.log("Show SignOut Button");
+}
+else{
+    console.log("Show login option");
+}
+
+
+(authenicate)?console.log("Show Signout"):console.log("Login Button");
+
+
