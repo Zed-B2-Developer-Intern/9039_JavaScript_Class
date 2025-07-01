29e6ed20d4d6ae948a0383b2706253f7fe07cce4
UmaaMageshwari <umaa0210ammu@gmail.com>
2025-07-01 17:18:01 +0530

    array

---
+++ array.js
@@ -0,0 +1,12 @@
+var country=["India","japan","UK","USA"];
+console.log(country);
+var states=["TamilNadu","Kerala","Karnataka","AndhraPradesh"];
+console.log(states);
+console.log(states[2])
+var user=new Array("Umaa","Umaa@gmail.com",3,true);
+console.log(user.length);
+console.log(states.pop());
+console.log(user.unshift("01"));
+console.log(user.shift());
+console.log(user.indexOf(0));
+console.log(Array.from("umaa"));
