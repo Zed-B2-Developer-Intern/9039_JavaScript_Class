fc19e04c19601ff08a4a64e375ff9ac0cb116c02
UmaaMageshwari <umaa0210ammu@gmail.com>
2025-07-01 18:35:54 +0530

    Object

---
+++ object.js
@@ -0,0 +1,14 @@
+var user={
+    firstName:"Umaa",
+    lastName:"Mageshwari",
+    role:"Admin",
+    loginCount:32,
+    facebook:true
+
+}
+console.log(user);
+console.log(user.firstName);
+user.loginCount=25;
+console.log(user);
+
+console.table(user);
